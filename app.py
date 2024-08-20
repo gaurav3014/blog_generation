db1dc15a71e1147e064cb313bad2f8497318f7ff
Gaurav N Pawar <78693210+gaurav3014@users.noreply.github.com>
2024-08-20 23:18:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 
-## Function To get response from LLAma 2 model
+# Function To get response from LLAma 2 model
 
 def getLLamaresponse(input_text,no_words,blog_style):
 
-    ### LLama2 model
+    
     llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
                       model_type='llama',
                       config={'max_new_tokens':256,
                               'temperature':0.01})
     
-    ## Prompt Template
+    
 
     template="""
         Write a blog for {blog_style} job profile for a topic {input_text}
@@ -22,7 +22,7 @@ def getLLamaresponse(input_text,no_words,blog_style):
     prompt=PromptTemplate(input_variables=["blog_style","input_text",'no_words'],
                           template=template)
     
-    ## Generate the ressponse from the LLama 2 model
+    # Generate the response from the LLama 2 model
     response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=no_words))
     print(response)
     return response
@@ -41,7 +41,6 @@ st.header("Generate Blogs 🤖")
 
 input_text=st.text_input("Enter the Blog Topic")
 
-## creating to more columns for additonal 2 fields
 
 col1,col2=st.columns([5,5])
 
@@ -53,6 +52,5 @@ with col2:
     
 submit=st.button("Generate")
 
-## Final response
 if submit:
     st.write(getLLamaresponse(input_text,no_words,blog_style))
