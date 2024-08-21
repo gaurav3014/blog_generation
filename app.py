5c719b9c319d9dc950d15f14712df3eb2edc6326
Gaurav N Pawar <78693210+gaurav3014@users.noreply.github.com>
2024-08-21 10:30:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.llms import CTransformers
 def getLLamaresponse(input_text,no_words,blog_style):
 
     
-    llm=CTransformers(model='north/north-mistral-7b-ggml',
+    llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
                       model_type='llama',
                       config={'max_new_tokens':256,
                               'temperature':0.01})
